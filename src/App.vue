ff129a026edada5cb91c7e07c4ff7e81767736d5
superDragon <admin@jixiaokang.com>
2020-09-14 13:49:24 +0800

    💄 feat:更新样式

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,21 @@
   <div id="app">
     <h1 style="text-align: center;">黑科技表格转化</h1>
     <Change></Change>
+     <footer class="footer">
+      Copyright © 冀小康 2020 by
+      <a href="https://github.com/xkloveme/black-tool" target="_blank"
+        >xkloveme</a
+      >
+      <iframe
+        style="margin-left: 2px; margin-bottom:-5px;"
+        frameborder="0"
+        scrolling="0"
+        width="91px"
+        height="20px"
+        src="https://ghbtns.com/github-btn.html?user=xkloveme&repo=black-tool&type=star&count=true"
+      >
+      </iframe>
+    </footer>
   </div>
 </template>
 
@@ -26,5 +41,15 @@ export default {
   width: 100%;
   height: 100%;
   min-height: 100vh;
+}
+.footer {
+  text-align: center;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  bottom: 5px;
+  left: 5px;
+  right: 5px;
 }
 </style>
