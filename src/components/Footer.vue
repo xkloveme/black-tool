dc6d345279ee0a36e7169078470549e232477d21
xkloveme <xkloveme@gmail.com>
2023-12-22 14:29:51 +0800

    feat；更新

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,14 @@
 <!--
  * @Date: 2023-12-20
- * @LastEditTime: 2023-12-20 11:28:54
+ * @LastEditTime: 2023-12-22 14:29:41
  * @LastEditors: xkloveme
  * @FileDesc:new page
- * @FilePath:/
+ * @FilePath: /black-tool/src/components/Footer.vue
  * @Copyright © xkloveme
 -->
 <template>
   <footer class="footer">
-    Copyright © 冀小康 2020 by
+    Copyright © 冀小康 2024 by
     <a href="https://github.com/xkloveme/black-tool" target="_blank">xkloveme</a>
     <iframe style="margin-left: 2px; margin-bottom:-5px;" frameborder="0" scrolling="0" width="91px" height="20px"
       src="https://ghbtns.com/github-btn.html?user=xkloveme&repo=black-tool&type=star&count=true">
